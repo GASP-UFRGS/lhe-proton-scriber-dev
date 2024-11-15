1f7fe750b4aeb89b8746355759c9009f577ca1a8
Gustavo Gil <5755070+diemort@users.noreply.github.com>
2024-11-15 12:12:48 -0300

    all improvements

--- proton_scriber.py
+++ proton_scriber.py
@@ -11,14 +11,16 @@ import setters
 Writes the protons into an LHE file from MadGraph or Superchic 
 '''
 
-def write_protons(_event,_sign,_generator):
+def write_protons(_event,_sign,_generator,_id1,_id2):
+    _ppos = _event.index('<mgrwt>\n')
     if _generator == 'superchic':
-        if _sign > 0: _event.insert(i+1, ' '*13+f'2212{" "*8}1    0    0    0    0 {px} {py} +{pzp:.9e}  {ep:.9e}  {m0:.9e} 0. 1.\n')
-        else: _event.insert(i+1, ' '*13+f'2212{" "*8}1    0    0    0    0 {px} {py} {pzp:.9e}  {ep:.9e}  {m0:.9e} 0. -1.\n')
+        if _sign > 0: _event.insert(_ppos, ' '*13+f'2212{" "*8}1    0    0    0    0 {px} {py} +{pzp:.9e}  {ep:.9e}  {m0:.9e} 0. 1.\n')
+        else: _event.insert(_ppos, ' '*13+f'{2212}{" "*8}1    0    0    0    0 {px} {py} {pzp:.9e}  {ep:.9e}  {m0:.9e} 0. -1.\n')
     if _generator == 'madgraph':
-        if _sign > 0: _event.insert(i+1, ' '*5+f'2212{" "*2}1    0    0    0    0 +{0:.11e} +{0:.11e} +{pzp:.11e}{" "*2}{ep:.11e}{" "*2}{m0:.11e} {0:.4e} {1:.4e}\n')
-        else: _event.insert(i+1, ' '*5+f'2212{" "*2}1    0    0    0    0 -{0:.11e} -{0:.11e} {pzp:.11e}{" "*2}{ep:.11e}{" "*2}{m0:.11e} {0:.4e} {-1:.4e}\n')
+        if _sign > 0: _event.insert(_ppos, ' '*5+f'{_id1}{" "*2}1    0    0    0    0 +{0:.10e} +{0:.10e} +{pzp:.10e}{" "*1}{ep:.10e}{" "*1}{m0:.10e} {0:.4e} {1:.4e}\n')
+        else: _event.insert(_ppos, ' '*5+f'{_id2}{" "*2}1    0    0    0    0 -{0:.10e} -{0:.10e} {pzp:.10e}{" "*1}{ep:.10e}{" "*1}{m0:.10e} {0:.4e} {-1:.4e}\n')
     # the 6th number is the xi, while px = py = m0 = 0, and spin/helicity = 9 for identification as a pileup proton
+    return _event
 
 if __name__ == "__main__":
     # Set proton mass
@@ -31,7 +33,7 @@ if __name__ == "__main__":
     # Gather definitions
     outputfile = 'new_'+inputfile
     printinvm = checkers.check_args(generator)
-    end,flag0,flag1,header,pzini,eini = setters.set_energy(inputfile,generator)
+    end,flag0,flag1,header,einip,einim,pzinip,pzinim,id1,id2 = setters.set_energy(inputfile,generator)
     # Start event processing
     event = []
     ofile = open(outputfile, 'w')
@@ -57,16 +59,20 @@ if __name__ == "__main__":
                                 px = f'{-eval(line[6]):.9e}' if -eval(line[6]) < 0 else f'+{-eval(line[6]):.9e}'
                                 py = f'{-eval(line[7]):.9e}' if -eval(line[7]) < 0 else f'+{-eval(line[7]):.9e}'
                                 pzf = eval(line[8])
+                                ef = eval(line[9])
                                 # Check proton direction via pz sign
                                 sign = (pzf/abs(pzf))
-                                pzp = pzini*sign - pzf
-                                ef = eval(line[9])
-                                ep = eini - ef
-                                # Add protons to the output file
-                                write_protons(event,sign,generator)
-                                # Check and adds pileup
-                                if (pileup == 'true' or pileup == "True"):
-                                    event = fill_pileup.fill_puprotons(i,event,generator,pzini,m0)
+                                if sign > 0:
+                                    pzp = pzinip*sign - pzf
+                                    ep = einip - ef
+                                else:
+                                    pzp = pzinim*sign - pzf
+                                    ep = einim - ef
+                                # Add protons to the output file if IDs given
+                                event = write_protons(event,sign,generator,id1,id2)
+                        # Check and adds pileup
+                        if (pileup == 'true' or pileup == "True"):
+                            event = fill_pileup.fill_puprotons(event,generator,pzinip,pzinim,m0,id1,id2)
                         for nl in event:
                             # Write new event to output file
                             ofile.write(nl)
--- setters.py
+++ setters.py
@@ -14,15 +14,31 @@ def set_energy(_inputfile,_generator):
         _end = ' </LesHouchesEvents>\n'
     # Create list of lines from the LHE file and if needed finding the beginning of events 
     with open(_inputfile, 'r+') as _f:
-        _line = _f.readline()
-        while _line != _header:
-            _line = _f.readline()
-        _beamenergy = int(float(_f.readline().split()[2]))
+        _lines = _f.readlines()
+        _index = _lines.index(_header)
+        _line = _lines[_index+1].split()
+        _ebeam_plus  = float(_line[2])
+        _ebeam_minus = float(_line[3])
+        if _generator == "madgraph":
+            _ionline1 = [_ion1 for _ion1 in _lines if "nb_proton1" in _ion1]
+            _zion1 = _ionline1[0].split()[0]
+            _ionline2 = [_ion2 for _ion2 in _lines if "nb_proton2" in _ion2]
+            _zion2 = _ionline2[0].split()[0]
+            print(_zion1+" --- "+_zion2)
+            _id1 = 2212 if int(_zion1) == 1 else 92212
+            _id2 = 2212 if int(_zion2) == 1 else 92212
+            print(str(_id1)+" --- "+str(_id2))
+            if int(_zion1) > 1 or int(_zion2) > 1:
+                print("This is a proton-ion collision")
+            if int(_zion1) >1 and int(_zion2) > 1:
+                print("This is a ion-ion collision")
     # Set energies:
-    _pzini = _beamenergy    # protons inicial pz 
-    _eini = _beamenergy     # protons inicial energy
+    _einip = _ebeam_plus         # protons initial energy z-pos
+    _einim = _ebeam_minus        # protons initial energy z-neg
+    _pzini_plus = _ebeam_plus    # protons initial pz positive 
+    _pzini_minus = _ebeam_minus  # protons initial pz negative
 
-    return _end,_flag0,_flag1,_header,_pzini,_eini
+    return _end,_flag0,_flag1,_header,_einip,_einim,_pzini_plus,_pzini_minus,_id1,_id2
 
 def set_proton_mass():
     # Get the proton mass in kg
