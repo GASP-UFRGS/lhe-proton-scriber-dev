cc27c54f9770485f91b60615a546a0c6409df4ba
Gustavo Gil <5755070+diemort@users.noreply.github.com>
2024-11-15 22:22:04 -0300

    correcting position of added protons in LHE file
    Signed-off-by: Gustavo Gil <5755070+diemort@users.noreply.github.com>

--- write_protons.py
+++ write_protons.py
@@ -4,12 +4,30 @@ def write_protons(_event,_sign,_generator,_id1,_id2,_px,_py,_pzp,_en,_mass):
     '''
     # check position reference
     _ppos = _event.index('<mgrwt>\n')
-    # inser lines
+    
+    # insert lines
     if _generator == 'superchic':
-        if _sign > 0: _event.insert(_ppos, ' '*13+f'2212{" "*8}1    0    0    0    0 {_px} {_py} +{_pzp:.9e}  {ep:.9e}  {m0:.9e} 0. 1.\n')
-        else: _event.insert(_ppos, ' '*13+f'{2212}{" "*8}1    0    0    0    0 {_px} {_py} {_pzp:.9e}  {ep:.9e}  {m0:.9e} 0. -1.\n')
+        if _sign > 0:
+            _event.insert(
+                _ppos,
+                ' ' * 13 + f'2212{" " * 8}1    0    0    0    0 {_px} {_py} +{_pzp:.9e}  {ep:.9e}  {m0:.9e} 0. 1.\n'
+            )
+        else:
+            _event.insert(
+                _ppos,
+                ' ' * 13 + f'2212{" " * 8}1    0    0    0    0 {_px} {_py} {_pzp:.9e}  {ep:.9e}  {m0:.9e} 0. -1.\n'
+            )
     if _generator == 'madgraph':
-        if _sign > 0: _event.insert(_ppos, ' '*5+f'{_id1}{" "*2}1    0    0    0    0 +{0:.10e} +{0:.10e} +{_pzp:.10e}{" "*1}{_en:.10e}{" "*1}{_mass:.10e} {0:.4e} {1:.4e}\n')
-        else: _event.insert(_ppos, ' '*5+f'{_id2}{" "*2}1    0    0    0    0 -{0:.10e} -{0:.10e} {_pzp:.10e}{" "*1}{_en:.10e}{" "*1}{_mass:.10e} {0:.4e} {-1:.4e}\n')
-    # the 6th number is the xi, while px = py = m0 = 0, and spin/helicity = 9 for identification as a pileup proton
+        if _sign > 0:
+            _event.insert(
+                _ppos,
+                f'{" " * (9 - len(str(_id1)))}{_id1}{" " * 2}1    0    0    0    0 +{0:.10e} +{0:.10e} +{_pzp:.10e} {" "}{_en:.10e} {" "}{_mass:.10e} {0:.4e} {1:.4e}\n'
+            )
+        else:
+            _event.insert(
+                _ppos,
+                f'{" " * (9 - len(str(_id2)))}{_id2}{" " * 2}1    0    0    0    0 -{0:.10e} -{0:.10e} {_pzp:.10e} {" "}{_en:.10e} {" "}{_mass:.10e} {0:.4e} {-1:.4e}\n'
+            )
+    
+    # The 6th number is the xi, while px = py = m0 = 0, and spin/helicity = 9 for identification as a pileup proton
     return _event
