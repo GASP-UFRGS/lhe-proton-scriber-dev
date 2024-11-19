53d5273efc065d9856c258aee0925dc476c45a60
Gustavo Gil <5755070+diemort@users.noreply.github.com>
2024-11-19 12:55:02 -0300

    adding setup for installation
    Signed-off-by: Gustavo Gil <5755070+diemort@users.noreply.github.com>

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="proton-scriber",
+    version="1.0.0",
+    install_requires=[
+        "numpy>=2.1.3",
+        "scipy>=1.14.1",
+        "tqdm>=4.6.7",
+    ],
+)
